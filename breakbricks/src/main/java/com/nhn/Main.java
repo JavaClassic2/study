cd86df462e18f5da93f372107e0e6d591e01a6ad
siddltkfkd <siddltkfkd213@gmail.com>
2024-08-21 16:39:50 +0900

    Refactor: BreakBricks extends World

--- breakbricks/src/main/java/com/nhn/Main.java
+++ breakbricks/src/main/java/com/nhn/Main.java
@@ -3,7 +3,7 @@ package com.nhn;
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
-import com.nhn.world.World;
+import com.nhn.world.BreakBricks;
 
 public class Main {
     private static final int FRAME_WIDTH = 1600;
@@ -11,16 +11,16 @@ public class Main {
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Break Bricks");
-        World world = new World(5);
-
         frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.add(world);
+        
+        BreakBricks breakBricks = new BreakBricks(5);
+        frame.add(breakBricks);
         frame.setVisible(true);
 
-        BreakBricks breakBricks = new BreakBricks(world);
+        breakBricks.prepareWorld();
         breakBricks.setBricks(10, 3);
 
-        world.run();
+        breakBricks.run();
     }
 }
--- breakbricks/src/main/java/com/nhn/world/BreakBricks.java
+++ breakbricks/src/main/java/com/nhn/world/BreakBricks.java
@@ -1,4 +1,4 @@
-package com.nhn;
+package com.nhn.world;
 
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
@@ -9,44 +9,37 @@ import com.nhn.circle.MovableBall;
 import com.nhn.rectangle.Bar;
 import com.nhn.rectangle.Brick;
 import com.nhn.rectangle.GameOverBlock;
-import com.nhn.world.World;
 
-public class BreakBricks {
-    private World world;
+public class BreakBricks extends World{
     private Bar bar;
     MovableBall ball;
 
-    public BreakBricks(World world) {
-        this.world = world;
-        prepareWorld();
+    public BreakBricks() {
+        super();
+    }
+    
+    public BreakBricks(int dt) {
+        super(dt);
     }
 
-    public int getWidth() {
-        return world.getWidth();
-    }
-
-    public int getHeight() {
-        return world.getHeight();
-    }
-
-    private void prepareWorld(){
+    public void prepareWorld(){
         bar = new Bar(getWidth()/2, getHeight()*8/9, getWidth()/5, getHeight()/50);
-        world.add(bar);
+        add(bar);
 
         MovableBall ball = new MovableBall(getWidth()/2, (getHeight()*8/9)-(getHeight()/50)*3/2, getHeight()/50);
-        world.add(ball);
+        add(ball);
 
         GameOverBlock overBlock = new GameOverBlock(getWidth()/2, getHeight()*49/50, getWidth(), getHeight()/50);
-        world.add(overBlock);
+        add(overBlock);
 
         addEvent();
     }
 
     private void addEvent() {
-        world.setFocusable(true);
-        world.requestFocus();
+        setFocusable(true);
+        requestFocus();
 
-        world.addKeyListener(new KeyAdapter() {
+        addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
                 super.keyPressed(e);
@@ -61,7 +54,7 @@ public class BreakBricks {
             }
         });
 
-        world.addMouseMotionListener(new MouseMotionAdapter() {
+        addMouseMotionListener(new MouseMotionAdapter() {
            @Override
            public void mouseDragged(MouseEvent e) {
                super.mouseDragged(e);
@@ -72,7 +65,7 @@ public class BreakBricks {
     }
 
     /**
-     * world 상단에 x * y 개의 벽돌을 d 간격으로 생성
+     * 상단에 x * y 개의 벽돌을 d 간격으로 생성
      * @param x 가로줄에 위치할 블록 갯수 
      * @param y 세로줄에 위치할 블록 갯수
      */
@@ -80,13 +73,13 @@ public class BreakBricks {
         for (int i=0; i<y; i++) {
             for (int j=0; j<x; j++) {
                 Brick brick = new Brick((getWidth()/2/x)*(1+2*j), getHeight()/20*(1+2*i), getWidth()/x-10, getHeight()/10-10);
-                world.add(brick);
+                add(brick);
             }
         }
     }
 
     /**
-     * world 상단에 x * y 개의 벽돌을 d 간격으로 생성
+     * 상단에 x * y 개의 벽돌을 d 간격으로 생성
      * @param x 가로줄에 위치할 블록 갯수
      * @param y 세로줄에 위치할 블록 갯수
      * @param d 벽돌 사이 간격 (가로, 세로 간격은 동일함)
@@ -98,7 +91,7 @@ public class BreakBricks {
         for (int i=0; i<y; i++) {
             for (int j=0; j<x; j++) {
                 Brick brick = new Brick((width/2)*(1+2*j), height/2*(1+2*i), width-d, height-d);
-                world.add(brick);
+                add(brick);
             }
         }
     }
